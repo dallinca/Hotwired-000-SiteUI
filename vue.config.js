f0af7d6ba401e229e3c919eb965ee2e7bd48e17a
Dallin Andersen <andersen.dallin@gmail.com>
2021-12-05 12:51:37 -0700

    adjust build path to hotwiredAPI project

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-const pathToRootPublic = "../../Node-001-JwtAuthSite/public/";
+const pathToRootPublic = "../Node-001-JwtAuthSite/public/";
 
 module.exports = {
     outputDir: pathToRootPublic
